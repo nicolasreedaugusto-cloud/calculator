366f3b1f2f1a3375e2086bbd90ccf43b5de4427c
Nicolas <nicolasreedaugusto@gmail>
2026-08-29 16:41:11 +0300

    Began calculator project. Set up simple design and am beguinning function set ups

---
+++ calc_script.js
@@ -0,0 +1,14 @@
+function variables() {
+    let one = document.querySelector('.number1')
+    let two = document.querySelector('.number2')
+}
+
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function devide() {}
+
+function operate() {}
